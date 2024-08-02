71884374de2c2cc5ab488694bb32e23b64e276cf
Bhagyashri9380 <bhagyashrimatiwaddar1991@gmail.com>
2024-08-02 20:10:24 +0530

    testing1

---
+++ src/main/java/com/airtel/AirteldemoApplication.java
@@ -0,0 +1,13 @@
+package com.airtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirteldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirteldemoApplication.class, args);
+	}
+
+}
